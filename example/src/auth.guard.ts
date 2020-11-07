2cc49f7e9ec84122309621d3b5ff4051a37955a6
Giangondo2806 <49293951+Giangondo2806@users.noreply.github.com>
2020-11-07 10:38:03 +0900

    a

--- example/src/auth.guard.ts
+++ example/src/auth.guard.ts
@@ -3,12 +3,12 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> {
-    const req = context.switchToHttp().getRequest();
+    const req = context.switchToHttp().getRequest<Request>() as any
     const fakeUser = {
       roles: ['ADMIN_UPDATE_OWN_VIDEO', 'USER_CREATE_ANY_VIDEO'],
       username: '@fake',
     };
-    req.user = fakeUser;
+    (req as any).user = fakeUser;
     return true;
   }
 }
